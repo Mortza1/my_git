8ae95cccff4b41e5f1a0e04c8cfb2de48064159b
murtaza <murtaza.0903@gmail.com>
2024-11-14 22:44:26 +0400

    initial commit

---
+++ libwyag.py
@@ -0,0 +1,17 @@
+import argparse
+import collections
+import configparser
+from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
+import os
+import re
+import sys
+import zlib
+
+argparser = argparse.ArgumentParser(description="The stupidist content tracker")
+argsubparsers = argparser.add_subparsers(title='Commands', dest='command')
+argsubparsers.required = True
+
